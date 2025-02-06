c88b5b54d2cc4db4dbcab6677cf55c75899b5a82
Siqueira69 <matheussiker@gmail.com>
2025-02-06 07:31:54 -0300

    Cafe Brasil

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+function insta() {
+    window.location.href = ("https://www.instagram.com");
+}
+
+function face() {
+    window.location.href = ("https://www.facebook.com/?locale=pt_BR");
+}
+
+function twitter() {
+    window.location.href = ("https://x.com/?lang=pt");
+}
